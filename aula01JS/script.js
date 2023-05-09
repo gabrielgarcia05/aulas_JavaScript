e3c5f1230d5ed456448e985cdd765edaa2c08f69
Gabriel Garcia <100931438+gabrielgarcia05@users.noreply.github.com>
2023-05-09 10:55:47 -0300

    Add files via upload

---
+++ aula01JS/script.js
@@ -0,0 +1,25 @@
+// var nome = prompt("Digite seu nome:")
+// console.log(nome)
+
+// alert("Bem vindo  " + nome + ", meu lindo!")
+
+// alert("Quebramos a maldiçao do hello world")
+// confirm("Você quer continuar?")
+// prompt("Digite ''sim'' para continuar:")
+
+// DECLARAR VARIÁVEIS
+
+// var ()
+// var a = 10
+// a = 20
+// console.log(a)
+
+// let (contexto de local ou global)
+// let a = 10
+// a = 20
+// console.log(a)
+
+// const (variável constante, não altera o valor recebido)
+// const a = 10
+// a = 20
+// console.log(a)
